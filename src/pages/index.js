66e5f3ad0df581ce1b4536ba0923b6adbc633177
delioncourts <delioncourtles@yandex.ru>
2022-10-03 01:58:32 +0300

    upd second-slide, index.js and scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,11 @@
 import './index.scss';
+
+/*Стол поднимается*/
+document.getElementById('button_up').onclick = function () {
+    document.getElementById('first__image_table').classList.toggle('first__image_up');
+}
+
+/*Стол опускается*/
+document.getElementById('button_down').onclick = function () {
+    document.getElementById('first__image_table').classList.toggle('first__image_down');
+}
